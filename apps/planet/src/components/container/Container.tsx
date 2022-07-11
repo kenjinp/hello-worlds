b6831b7579f99d92c3d5c81a56d172f79ccc988c
Kenneth Pirman <github@kenny.wtf>
2022-07-11 23:34:49 +0200

    fix: click to close mobile

--- apps/planet/src/components/container/Container.tsx
+++ apps/planet/src/components/container/Container.tsx
@@ -8,15 +8,32 @@ export const ContainerDiv = styled.div`
   border-radius: 0.5em;
   box-shadow: 0 16px 40px -5px rgb(0 0 0 / 50%);
   background: #2f2f2f;
-  .dragger {
+  .dragger-bounds {
     background: #dd3cff29;
     border-top-left-radius: 0.5em;
     border-top-right-radius: 0.5em;
     height: 2em;
     cursor: grab;
     transition: all 0.3s ease;
+    & svg {
+      fill: white;
+    }
     &:hover {
       background: #dd3cff9c;
+      & svg {
+        fill: white;
+      }
+    }
+    .dragger {
+      width: 100%;
+      margin: 0 auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top: 10px;
+      left: 0px;
+      z-index: 1;
     }
   }
   .content {
@@ -35,9 +52,9 @@ export const Container: React.FC<
   }>
 > = ({ style, children, header, onClose }) => {
   return (
-    <Draggable handle=".dragger" bounds="#window-bounds" min>
+    <Draggable handle=".dragger" bounds="#window-bounds">
       <ContainerDiv style={style}>
-        <div className="dragger">
+        <div className="dragger-bounds">
           <div
             style={{
               display: "flex",
@@ -46,9 +63,24 @@ export const Container: React.FC<
             }}
           >
             <div>{header}</div>
+            <div className="dragger">
+              <svg
+                width="20"
+                height="10"
+                viewBox="0 0 28 14"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="2" cy="2" r="2"></circle>
+                <circle cx="14" cy="2" r="2"></circle>
+                <circle cx="26" cy="2" r="2"></circle>
+                <circle cx="2" cy="12" r="2"></circle>
+                <circle cx="14" cy="12" r="2"></circle>
+                <circle cx="26" cy="12" r="2"></circle>
+              </svg>
+            </div>
             <div
               onClick={() => onClose && onClose()}
-              style={{ cursor: "pointer" }}
+              style={{ cursor: "pointer", zIndex: 2 }}
             >
               <FontAwesomeIcon icon={faXmark} />
             </div>
